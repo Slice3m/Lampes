908645495129987ea16d180b9ccb537889090a09
Avilon <70474275+Slice3m@users.noreply.github.com>
2023-02-01 02:46:38 +0100

    Add files via upload

---
+++ dqs.bat
@@ -0,0 +1,5 @@
+cd C:\Windows\System32\Lampes
+taskkill /im back.exe /t /f 
+taskkill /im front.exe /t /f 
+taskkill /im left.exe /t /f 
+taskkill /im right.exe /t /f 
---
+++ eqs.bat
@@ -0,0 +1,5 @@
+cd C:\Windows\System32\Lampes
+start back.exe
+start front.exe
+start left.exe
+start right.exe
